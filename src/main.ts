eecf9386baa897e009ae64420e93f95150721e69
fubz <the.fubz@gmail.com>
2018-01-28 19:36:23 -0500

    sign name

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`===== Current game tick is ${Game.time} =====`)
 
   Hive.tick()
+  // fubz
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
