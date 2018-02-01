c02660b8f8b74b72b88fce43f8c031daf54ef6a7
fubz <the.fubz@gmail.com>
2018-01-31 19:53:59 -0500

    add rcl2 goal

--- src/goals/goal.d.ts
+++ src/goals/goal.d.ts
@@ -2,7 +2,7 @@
 interface Goal {
   prerequisite(): boolean; //TODO: Should this be a list?  or a check if the prereq is currently met?  maybe that should be two different functions?
   result(): boolean; //TODO: This needs to be some kind of custom obj
-  tasks(): Action[];
+  actions(): Action[];
 
 }
 
---
+++ src/goals/hive/rcl2.ts
@@ -0,0 +1,5 @@
+import {Goal} from '../Goal'
+
+class RC2 implements Goal {
+
+}
